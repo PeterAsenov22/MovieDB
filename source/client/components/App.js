64a3f889376c7e3b9de37328952b954e881eac08
pesho <peter12345@abv.bg>
2018-06-06 16:33:00 +0300

    App component modified

--- source/client/components/App.js
+++ source/client/components/App.js
@@ -1,64 +1,33 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import UserStore from '../stores/UserStore'
+import UserActions from '../actions/UserActions'
 
 export default class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-      loggedInUserId: ''
-    }
+    this.state = UserStore.getState()
 
-    this.LOGIN_DEFAULT_USER = this.LOGIN_DEFAULT_USER.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange (state) {
+    this.setState(state)
   }
 
   componentDidMount () {
-    this.LOGIN_DEFAULT_USER()
+    UserStore.listen(this.onChange)
+    UserActions.loginUser()
   }
 
-  LOGIN_DEFAULT_USER () {
-    let request = {
-      url: '/user/login',
-      method: 'post',
-      data: JSON.stringify({username: 'admin', password: 'admin'}),
-      contentType: 'application/json'
-    }
-
-    $.ajax(request)
-      .done(userId => {
-        this.setState({loggedInUserId: userId})
-      })
-      .fail(err => {
-        console.log('Error: ' + err)
-        this.setState({
-          loggedInUserId: '',
-          message: err.responseJSON.message
-        })
-      })
-  }
-
-  logoutUser () {
-    let request = {
-      url: '/user/logout',
-      method: 'post'
-    }
-
-    $.ajax(request)
-      .done(() => {
-        this.setState({loggedInUserId: ''})
-      })
-      .fail(err => {
-        this.setState({
-          error: err.responseJSON.message
-        })
-      })
+  componentWillUnmount () {
+    UserStore.unlisten(this.onChange)
   }
 
   render () {
     let userData = {
-      loggedInUserId: this.state.loggedInUserId,
-      loginUser: this.LOGIN_DEFAULT_USER,
-      logoutUser: this.logoutUser.bind(this)
+      loggedInUserId: this.state.loggedInUserId
     }
     return (
       <div>
